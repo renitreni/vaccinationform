6df5e87e62fd692b222faf5e560d069689339f11
Renier Trenuela <rtrenuela@objectbright.com>
2021-08-05 13:58:26 +0800

    update path

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,7 +7,7 @@
     <title>{{ env('APP_NAME') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('vendor/adminkit/dist/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminkit/static/css/app.css') }}" rel="stylesheet">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <link href="{{ asset('vendor/alertifyjs/css/alertify.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/alertifyjs/css/themes/bootstrap.css') }}" rel="stylesheet">
@@ -25,7 +25,7 @@
     </div>
 </div>
 
-<script src="{{ asset('vendor/adminkit/dist/js/app.js') }}"></script>
+<script src="{{ asset('vendor/adminkit/static/js/app.js') }}"></script>
 <script src="{{ asset('vendor/alertifyjs/alertify.min.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
